3f2921beff073a96c214c16bf51f609eb1ca9c8d
Ysherlin <ysherlin.govender@gmail.com>
2021-09-26 20:07:19 +0200

    Add files via upload

---
+++ Currency Converter/Rate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_Converter
+{
+	//Make sure API return value names and where you want to store that name are same. Like in API get response.
+	public class Rate
+	{
+		public double INR { get; set; }
+		public double JPY { get; set; }
+		public double USD { get; set; }
+		public double NZD { get; set; }
+		public double EUR { get; set; }
+		public double CAD { get; set; }
+	}
+}
---
+++ Currency Converter/Root.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_Converter
+{
+	//Root Class is a main class. API returns rates in a rates it will return all currency name with value.
+	public class Root
+	{
+		//Get all Record in rates and set in Rate class as currency name wise.
+		public Rate rates { get; set; }
+		public long timestamp;
+		public string licence;
+	}
+}
